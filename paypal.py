73c9395587c6582024f42df2bf026c0b63890d2e
kishore <kishorekumarnetala@gmail.com>
2016-04-24 16:03:33 +0530

    commit1 for branch1

--- paypal.py
+++ paypal.py
@@ -8,3 +8,4 @@ paypal_url = 'https://api.sandbox.paypal.com/v1/oauth2/token'
 headers = {'Accept': 'application/json', 'Accept-Language': 'en_US', 'content-type': 'application/x-www-form-urlencoded'}
 params = {'grant_type': 'client_credentials'}
 response = requests.post(paypal_url, headers = headers, params = params, auth = HTTPBasicAuth(paypal_userid, paypal_secret))
+this is response text
